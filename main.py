05eaf45c823365db856de1ce8d72a6822b237d90
doczimilan3 <94104338+doczimilan3@users.noreply.github.com>
2022-04-07 09:18:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,38 +60,5 @@ def f2():
         print(s, "\n")
     os.system(v.kép)
 
-def f3():
-    orszag = str(input("Adj meg egy országot ahonnan nevezetességekről szeretnél olvasni: "))
-    helyin = []
-
-    for i in nevezetességek:
-        print(i.hely, "-", orszag)
-        if i.hely == orszag:
-            helyin.append(i)
-
-    print("\n")
-
-    for s in range(len(helyin)):
-        print(str(s+1) + " - " + helyin[s].név)
-
-    if len(helyin) == 0:
-        print("Nincs egy darab nevezetesség sem a megadott országban")
-        return
-
-    valasztas = int(input("Válassz egy nevezetességet amiről szeretnél többet tudni (1-" + str(len(helyin)) + ")): "))
-    while valasztas < 1 or valasztas > len(helyin):
-        valasztas = int(input("Válassz egy nevezetességet amiről szeretnél többet tudni (1-" + str(len(helyin)) + ")): "))
-    print("\n\n")
-    v = helyin[valasztas-1]
-    print(v.név, "\n")
-    print("Helyszín:", v.hely)
-    print("Keletkezés dátuma:", v.dátum)
-    print("Egyéb tudnivalók:\n")
-    l = open(v.leírás, "r", encoding="utf-8", errors="ignore")
-    for s in l:
-        print(s, "\n")
-    os.system(v.kép)
-
 f1()
 f2()
-f3()
