30bbf1daf13f3c81da9ca06d2c1fe9be9a50981c
doczimilan3 <94104338+doczimilan3@users.noreply.github.com>
2022-04-07 09:10:48 +0200

    Add files via upload

--- n_class.py
+++ n_class.py
@@ -2,6 +2,6 @@ class Nevezetesség:
     def __init__(self, név, hely, dátum, kép, leírás):
         self.név = név
         self.hely = hely
-        self.dátum = dátum
+        self.dátum = int(dátum)
         self.kép = kép
         self.leírás = leírás
